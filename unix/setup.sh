54614603003e1fa96ac1ec40f3424da4bd354e45
Shepherd Kendall <s@kndl.org>
2017-10-27 13:21:26 -0400

    added line numbers to vim

--- unix/setup.sh
+++ unix/setup.sh
@@ -25,6 +25,7 @@ set autowrite
 set t_Co=256
 set tabstop=8 softtabstop=0 expandtab shiftwidth=4 smarttab
 set background=dark
+set number
 let g:go_highlight_types = 1
 let g:go_highlight_fields = 1
 let g:go_highlight_functions = 1
