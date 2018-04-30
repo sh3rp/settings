6c51741b73da386bbcc198e92fc119f7ffdb741c
Shepherd Kendall <s@kndl.io>
2018-04-30 10:17:52 -0400

    added git setup

---
+++ unix/setup-scripts/git/setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+function install_git() {
+    git config --global user.name "Shepherd Kendall"
+    git config --global user.email "s@kndl.io"
+    git config --global core.editor vim
+    git config --global credential.helper cache
+}
