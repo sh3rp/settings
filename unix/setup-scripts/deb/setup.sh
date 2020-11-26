584da3eb8f30ec7f50061c16b90728addc25251e
Shepherd Kendall <s@kndl.io>
2020-11-26 20:43:37 +0000

    added gcc and net-tools

--- unix/setup-scripts/deb/setup.sh
+++ unix/setup-scripts/deb/setup.sh
@@ -2,6 +2,6 @@
 
 function install_deb() {
     sudo apt -y update && sudo apt -y upgrade
-    sudo apt -y install software-properties-common tmux python3-dev python3 autoconf make libssl-dev
+    sudo apt -y install gcc net-tools software-properties-common tmux python3-dev python3 autoconf make libssl-dev
     wget -O $HOME/.tmux.conf https://raw.githubusercontent.com/hamvocke/dotfiles/master/tmux/.tmux.conf
 }
